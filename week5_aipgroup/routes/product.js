7a21233e778b3f253eab437697269362c3cdbfc6
shawnkim8332 <seunghwan.kim@student.uts.edu.au>
2017-08-22 19:38:51 +1000

    Shawn created db connection object

--- week5_aipgroup/routes/product.js
+++ week5_aipgroup/routes/product.js
@@ -2,42 +2,28 @@ var express = require('express');
 var router = express.Router();
 //var app = express();
 var mysql      = require('mysql');
+var getConnection = require('./db');
 
 
 router.get("/product/list",function(req,res){
 
-    var con = mysql.createConnection({
-        host     : 'ourcityshop.com.au',
-        user     : 'ourcity9_aip2017',
-        password : 'Minmin@1',
-        database : 'ourcity9_aip2017_uts'
-    });
-
-    con.connect(function(err) {
+    getConnection(function (err, con) {
         if (err) throw err;
-        con.query("SELECT p.product_id, p.product_nm, p.description FROM ourcity9_aip2017_uts.product as p", function (err, rows, fields) {
+        con.query("SELECT p.product_id  , p.product_nm, p.description FROM aipgroup.product as p", function (err, rows, fields) {
             if (err){
                 console.log('Error while performing Query.');
+                con.release();
                 return res.send(err);
             }
             return res.json(rows);
         });
     });
-
 });
 
 router.get("/product/:id",function(req,res){
-
-    var con = mysql.createConnection({
-        host     : 'ourcityshop.com.au',
-        user     : 'ourcity9_aip2017',
-        password : 'Minmin@1',
-        database : 'ourcity9_aip2017_uts'
-    });
-
-    con.connect(function(err) {
+    getConnection(function (err, con) {
         if (err) throw err;
-        var sql = "SELECT p.product_id  , p.product_nm, p.description FROM ourcity9_aip2017_uts.product as p where p.product_id = ?";
+        var sql = "SELECT p.product_id  , p.product_nm, p.description FROM aipgroup.product as p where p.product_id = ?";
         var values = [req.params.id];
         con.query(sql, [values], function (err, rows, fields) {
             if (err){
@@ -45,49 +31,29 @@ router.get("/product/:id",function(req,res){
                 return res.send(err);
             }
             console.log('product data retrieved');
+            con.release();
             return res.json(rows);
         });
     });
-
 });
 
 router.post('/product/update/:id', function(req, res, next){
-
     var product = req.body;
-
-
-
-    var con = mysql.createConnection({
-        host     : 'ourcityshop.com.au',
-        user     : 'ourcity9_aip2017',
-        password : 'Minmin@1',
-        database : 'ourcity9_aip2017_uts'
-    });
-
-    con.connect(function(err) {
+    getConnection(function (err, con) {
         if (err) throw err;
-        var sql = "UPDATE ourcity9_aip2017_uts.product set product_nm = ?  , description = ?  where product_id = ?";
+        var sql = "UPDATE aipgroup.product set product_nm = ?  , description = ?  where product_id = ?";
         var values = [product.product_nm, product.description, product.product_id];
         console.log(values);
         con.query(sql, values, function (err, result) {
             if (err) throw err;
-            console.log(result.affectedRows + " record(s) updated");
-            res.json();
+            console.log(result.affectedRows + " record updated");
+            return res.json();
         });
     });
 });
 
-
 router.get("/flavor/:id",function(req,res){
-
-    var con = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'aipgroup',
-        password : 'aipgroup',
-        database : 'aipgroup'
-    });
-
-    con.connect(function(err) {
+    getConnection(function (err, con) {
         if (err) throw err;
         var sql = "SELECT f.flavor_id  , f.flavor_nm FROM aipgroup.flavor as f where f.product_id = ?";
         var values = [req.params.id];
@@ -99,19 +65,10 @@ router.get("/flavor/:id",function(req,res){
             return res.json(rows);
         });
     });
-
 });
 
 router.get("/ingredient/:id",function(req,res){
-
-    var con = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'aipgroup',
-        password : 'aipgroup',
-        database : 'aipgroup'
-    });
-
-    con.connect(function(err) {
+    getConnection(function (err, con) {
         if (err) throw err;
         var sql = "SELECT i.ingred_id  , i.ingred_nm  , q.quantity  , q.unit from aipgroup.ingredient as i  , aipgroup.ingred_qty as q where i.ingred_id = q.ingred_id and   q.flavor_id = ?";
         var values = [req.params.id];
@@ -123,9 +80,6 @@ router.get("/ingredient/:id",function(req,res){
             return res.json(rows);
         });
     });
-
 });
-
-
 
 module.exports = router;
